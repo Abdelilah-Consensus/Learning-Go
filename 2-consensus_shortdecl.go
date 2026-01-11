e57125e6ed6d81073bd7ef2f25578f90280c7196
Abdelilah <contact.chaindecoded@gmail.com>
2026-01-11 19:41:24 +0100

    Update 2-consensus_shortdecl.go

--- 2-consensus_shortdecl.go
+++ 2-consensus_shortdecl.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 
+// Definition of a variable: Block creation time
+
 var blockTime int64
 
 func main() {
+
+// Create a new function inside main
 
 voteRound()
 
 }
 
+// Calling the new function
+
 func voteRound() {
 
 round := 2  
 votes := 6  
-hasMajority:= votes/2 + 2  
+hasMajority := votes/2 + 2  
   
 fmt.Println("Round :", round)  
 fmt.Println("Votes :", votes)  
